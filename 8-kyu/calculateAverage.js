c93c768f8964e1ffe9541dc2ddd4ac86407c147c
Vadim Gavrish <vadim@gavrish.ca>
2017-07-16 21:04:54 +0000

    better solution for calculateAverage exercise

--- 8-kyu/calculateAverage.js
+++ 8-kyu/calculateAverage.js
@@ -1,13 +1,11 @@
 function find_average(array) {
 
     var sum = 0;
-    var counter = 0;
 
     for (var i = 0; i < array.length; i++) {
         sum  = sum + array[i];
-        counter++;
     }
 
-    var result = sum / counter;
+    var result = sum / array.length;
     return result;
 }
